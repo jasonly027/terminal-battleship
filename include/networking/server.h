9267deaacb44e74e9bec54e7dbd145ffbf440323
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2024-03-28 16:48:18 -0700

    Prepare to define server's virtual methods

--- include/networking/server.h
+++ include/networking/server.h
@@ -4,12 +4,21 @@
 
 #include "generic_net.h"
 #include "message_types.h"
+#include <memory>
 
 namespace battleship {
 class Server : public server_interface<MessageType> {
 public:
     Server(uint16_t port);
+
+protected:
+    virtual bool
+        on_client_connect(std::shared_ptr<connection<MessageType>>) override;
+    virtual void on_client_disconnect(
+        std::shared_ptr<connection<MessageType>> client) override;
+    virtual void on_message(std::shared_ptr<connection<MessageType>> client,
+                            message<MessageType> &msg) override;
 };
-}
+} // namespace battleship
 
 #endif
--- src/networking/server.cpp
+++ src/networking/server.cpp
@@ -1,6 +1,21 @@
 #include "server.h"
 #include <cstdint>
+#include <iostream>
+#include <memory>
 
 using namespace battleship;
 
 Server::Server(uint16_t port) : server_interface(port) {}
+
+bool Server::on_client_connect(std::shared_ptr<connection<MessageType>> client) {
+    std::cout << "Client is here\n";
+    return true;
+}
+
+void Server::on_client_disconnect(std::shared_ptr<connection<MessageType>> client) {
+    std::cout << "Client left\n";
+}
+
+void Server::on_message(std::shared_ptr<connection<MessageType>> client, message<MessageType> &msg) {
+
+}
